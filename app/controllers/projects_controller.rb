2bf7dd7b429cbb6a699697cebf0bf2eb1a794c1c
akioGitHub <akio.k2020@outlook.jp>
2022-08-18 20:40:23 +0900

    プロジェクト投稿ページ作成

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,4 +4,7 @@ class ProjectsController < ApplicationController
 
   def new
   end
+
+  def create
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,8 @@
 class Project < ApplicationRecord
   has_many :users, through: :affiliations
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :prefecture
+  belongs_to :select_donation
+
 end
