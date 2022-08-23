ffe6f53ded969e62e1ca073c0f2b492b8aac35a3
akioGitHub <akio.k2020@outlook.jp>
2022-08-23 18:54:42 +0900

    プロジェクト詳細にtaskボタン設置

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: "projects#index"
   resources :users, only: [:edit, :update, :show]
   resources :projects, only: [:new, :create, :show] do
+    resources :tasks, only: [:index, :new]
     resources :donations, only: [:index, :create]
     resources :rooms, only: [:new, :create, :destroy] do
       resources :messages, only: [:index, :create]
